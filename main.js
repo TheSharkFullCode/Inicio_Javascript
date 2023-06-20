ba77f9c9a0cdaa4067000709426da4f7f25d6d4e
TheSharkFullCode <agedjoscar@hotmail.com>
2023-06-21 01:09:16 +0200

    done

---
+++ main.js
@@ -0,0 +1,66 @@
+// const edad = 32;
+// let puerto = 21;
+// var numero = 58;
+
+
+
+
+
+
+// console.log(numero);
+// console.log(puerto);
+// console.log(edad);
+
+
+
+
+
+
+
+console.log(typeof 'JavaScript') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof "") // string
+console.log(typeof false) // boolean
+console.log(typeof 3.45) // number
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
+
+
+
+
+
+// const numero = 32;
+// const numero2 = 21;
+
+// result = "";
+
+// result = numero * numero2;
+
+// alert(result);
+
+
+let nombre = "", result;
+
+prueba = prompt('Cual es tu nombre:');
+
+nombre = prompt("introduce tu nombre");
+
+
+const number1 = 180;
+        number2 = 200;
+
+
+        result = number1 * number2;
+
+        alert("el resultado de la multiplicacion es "+result +"\n y tu nombre es:"+nombre);
+
+
+let prueba = "";
+console.log(prueba);
+
+
+
+
+
---
+++ variable.js
@@ -0,0 +1,99 @@
+let nombre = "Angel";
+let numero = "15";   //aqui le estoy diciendo que numero es un texto con valor(15);
+let  datos = true;  //aqui estoy almacenando datos con un valor Booleano. para comprobar si la consola lo detecta como tal.
+
+const num1 = 20, a = 2; //aqui estoy declarando varias lineas;
+
+    var madrid = ""; //
+
+    const MADRID = "";
+
+    //const genero = masculino;
+
+    
+    
+    
+    
+    
+    
+    console.log(typeof 'nombre');    //me da como valor (string);
+    
+    console.log(typeof 15);             //Me da como Valor (num);
+    
+    console.log(typeof numero);         //Aqui tambien me da como valor (string)
+    
+    console.log(typeof datos);
+    
+    console.log(typeof true);   
+    
+    console.log(typeof madrid);
+    console.log(typeof MADRID);
+    
+    //const madrid = masculino; 
+    
+    
+    
+    
+    //Error de Sintaxis: Falta el inicializador en la variable:(const).
+    
+        //const edad;    
+    
+        var apellidos;   
+    
+        //let false;  
+
+        // tambien en let me dio como error
+
+
+        //const name = oscar;
+        // aqui tambien me sugio un error en la variable (const) al asignarle por valor oscar
+
+         const surname = true;    //aqui me la da como verdadera
+     
+
+        //  tambien tengo un error
+        //  let casa = feliz;
+
+         var house = false;
+         console.log(typeof surname);
+
+         let mentira = true;
+
+         console.log(typeof mentira);
+
+
+
+        //  let firtsname = 'oscar';
+
+        //  var secondsname = 'rodriguez';
+
+        // let city = 'madrid';
+
+        // let age = 32;
+
+        // console.log(typeof age);
+
+        // let secongAge = '23';
+
+        // let country = 'spain';
+
+        let firtsname, secondsname, city, age,  country;
+
+
+        let miEdad = 'tengo 25 años';
+        let tuEad = 'tu tienes 30 años';
+
+        console.log(miEdad);
+        console.log(tuEad);
+
+        let numberOne;
+
+        numberOne = parseInt(prompt("introduce un numero entero sin decimales:"))
+
+        alert("tu numero es:"+numberOne);
+
+
+
+         
+
+    
